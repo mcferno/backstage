290a04cffb2b3cc6065ab32d32809b1cc94f9b1f
Patrick McFern <mcferno@gmail.com>
2011-12-27 00:33:16 -0500

    Adding live credentials + detection

--- Config/core.php
+++ Config/core.php
@@ -33,6 +33,10 @@
  * In development mode, you need to click the flash message to continue.
  */
 	Configure::write('debug', 2);
+	
+	if(stripos($_SERVER['HTTP_HOST'],'kennyquotemachine.com') !== false) {
+		Configure::write('debug', 0);
+	}
 
 /**
  * Configure the Error handler used to handle errors for your application.  By default
--- Config/database.php
+++ Config/database.php
@@ -10,6 +10,15 @@ class DATABASE_CONFIG {
 		'database' => 'kennyquotemachine',
 		'encoding' => 'utf8'
 	);
+	public $live = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'mcferno_kqm',
+		'password' => 'J4z_!3{@!3l~?,ufAk',
+		'database' => 'mcferno_kqm',
+		'encoding' => 'utf8'
+	);
 	public $test = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
@@ -19,4 +28,10 @@ class DATABASE_CONFIG {
 		'database' => 'testbed_test',
 		'encoding' => 'utf8'
 	);
+	
+	function __construct() {
+		if(stripos($_SERVER['HTTP_HOST'],'kennyquotemachine.com') !== false) {
+			$this->default = $this->live;
+		}
+	}
 }
