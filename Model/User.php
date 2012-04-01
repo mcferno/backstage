783dd16e462914df66bf1c52b205b2f863be6bd6
Patrick McFern <mcferno@gmail.com>
2012-04-01 13:11:29 -0400

    Relaxing the username requirements, while improving validation on it

--- Model/User.php
+++ Model/User.php
@@ -1,57 +1,37 @@
 <?php
 App::uses('AppModel', 'Model');
 App::uses('AuthComponent', 'Controller/Component');
-/**
- * User Model
- *
- */
+
 class User extends AppModel {
-/**
- * Display field
- *
- * @var string
- */
+
 	public $displayField = 'username';
-/**
- * Validation rules
- *
- * @var array
- */
+
 	public $validate = array(
 		'username' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),
 				'message' => 'Username is required',
-				'required' => true,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				'required' => true
 			),
-			'userdefined' => array(
-				'rule' => '/^[a-z0-9-]+?$/',
-				'message' => 'Lowercase, numbers and dashes \'-\' only.',
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			'formatting' => array(
+				'rule' => '/^[a-zA-Z0-9-_]{1,20}$/',
+				'message' => 'Uppercase, lowercase, numbers, underscores, and dashes only.',
+			),
+			'isUnique' => array(
+				'rule' => 'isUnique',
+				'message' => 'Sorry, that username is already taken.',
 			),
 		),
 		'password' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				'message' => 'Password is required'
 			),
 		),
 		'role' => array(
 			'numeric' => array(
 				'rule' => array('numeric'),
-				'message' => 'User role must be specified',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				'message' => 'User role must be specified'
 			),
 		),
 	);
